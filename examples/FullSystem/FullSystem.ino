314f8866c788ebd26ec55e2760c3bb1512459118
NachtRaveVL <nachtravevl@gmail.com>
2022-07-24 00:53:39 -0700

    last set of updated before merge, updates to examples to make them easier to deal with, documentation updates

--- examples/FullSystem/FullSystem.ino
+++ examples/FullSystem/FullSystem.ino
@@ -1,12 +1,14 @@
-// Simple-Hydroponics-Arduino Vertical Nutrient Film Technique (NFT) Example
+// Simple-Hydroponics-Arduino Full System
+// This sketch will build the entire library onto a device, while supporting all of its
+// functionality, and thus has the highest cost. Not meant for constrained devices.
 // TODO
 
 #include <Hydroponics.h>
 
 // Pins & Class Instances
 #define SETUP_PIEZO_BUZZER_PIN      -1              // Piezo buzzer pin, else -1
-#define SETUP_EEPROM_DEVICE_SIZE    I2C_DEVICESIZE_24LC256 // EEPROM bit storage size (use I2C_DEVICESIZE_* defines), else 0
-#define SETUP_SD_CARD_CS_PIN        SS              // SD card CS pin, else -1
+#define SETUP_EEPROM_DEVICE_SIZE    0               // EEPROM bit storage size (use I2C_DEVICESIZE_* defines), else 0
+#define SETUP_SD_CARD_CS_PIN        -1              // SD card CS pin, else -1
 #define SETUP_CTRL_INPUT_PIN1       -1              // First pin of input ribbon, else -1 (ribbon pins can be individually customized later)
 #define SETUP_EEPROM_I2C_ADDR       B000            // EEPROM address
 #define SETUP_RTC_I2C_ADDR          B000            // RTC i2c address (only B000 can be used atm)
@@ -18,7 +20,7 @@
 
 // System Settings
 #define SETUP_RUN_MODE              Recycling       // System run mode (Recycling, DrainToWaste)
-#define SETUP_MEASURE_MODE          Imperial        // System measurement mode (Default, Imperial, Metric, Scientific)
+#define SETUP_MEASURE_MODE          Default         // System measurement mode (Default, Imperial, Metric, Scientific)
 #define SETUP_LCD_OUT_MODE          Disabled        // System LCD output mode (Disabled, 20x4LCD, 20x4LCD_Swapped, 16x2LCD, 16x2LCD_Swapped)
 #define SETUP_CTRL_IN_MODE          Disabled        // System control input mode (Disabled, 2x2Matrix, 4xButton, 6xButton, RotaryEncoder)
 #define SETUP_SYS_NAME              "Hydruino"      // System name
@@ -31,53 +33,10 @@
 #define SETUP_WIFI_PASS             "CHANGE_ME"     // WiFi password
 
 // Logging & Data Publishing Settings
-#define SETUP_LOG_SD_ENABLE         true            // Whenever or system logging is enabled to SD card
+#define SETUP_LOG_SD_ENABLE         false           // Whenever or system logging is enabled to SD card
 #define SETUP_LOG_FILE_PREFIX       "logs/hy"       // System logs file prefix (appended with YYMMDD.txt)
-#define SETUP_DATA_SD_ENABLE        true            // Whenever or system data publishing is enabled to SD card
+#define SETUP_DATA_SD_ENABLE        false           // Whenever or system data publishing is enabled to SD card
 #define SETUP_DATA_FILE_PREFIX      "data/hy"       // System data publishing files prefix (appended with YYMMDD.csv)
-
-// Device Pin Setup
-#define SETUP_PH_METER_PIN          A0              // pH meter sensor pin (analog), else -1
-#define SETUP_TDS_METER_PIN         A1              // TDS meter sensor pin (analog), else -1
-#define SETUP_CO2_METER_PIN         -1              // CO2 meter sensor pin (analog), else -1
-#define SETUP_POWER_METER_PIN       -1              // Power meter sensor pin (analog), else -1
-#define SETUP_DS18_WTEMP_PIN        3               // DS18* water temp sensor data pin (digital), else -1
-#define SETUP_DHT_ATEMP_PIN         4               // DHT* air temp sensor data pin (digital), else -1
-#define SETUP_VOL_FILLED_PIN        -1              // Water level filled indicator pin (digital), else -1
-#define SETUP_VOL_EMPTY_PIN         -1              // Water level empty indicator pin (digital), else -1
-#define SETUP_GROW_LIGHTS_PIN       22              // Grow lights relay pin (digital), else -1
-#define SETUP_AERATOR_PIN           24              // Aerator relay pin (digital), else -1
-#define SETUP_FEED_PUMP_PIN         26              // Water level low indicator pin, else -1
-#define SETUP_WATER_HEATER_PIN      -1              // Water heater relay pin (digital), else -1
-#define SETUP_FAN_EXHAUST_PIN       -1              // Fan exhaust relay pin (digital), else -1
-#define SETUP_NUTRIENT_MIX_PIN      23              // Nutrient premix peristaltic pump relay pin (digital), else -1
-#define SETUP_FRESH_WATER_PIN       25              // Fresh water peristaltic pump relay pin (digital), else -1
-#define SETUP_PH_UP_PIN             27              // pH up solution peristaltic pump relay pin (digital), else -1
-#define SETUP_PH_DOWN_PIN           29              // pH down solution peristaltic pump relay pin (digital), else -1
-
-// Object Setup
-#define SETUP_FEED_RESERVOIR_SIZE   4               // Reservoir size, in default measurement units
-#define SETUP_AC_POWER_RAIL_TYPE    AC110V          // Rail power type used for AC rail (AC110V, AC220V)
-#define SETUP_DC_POWER_RAIL_TYPE    DC12V           // Rail power type used for peristaltic pump rail (DC5V, DC12V)
-
-// Crop Setup
-#define SETUP_CROP_ON_TIME          15              // Minutes feeding pumps are to be turned on for
-#define SETUP_CROP_OFF_TIME         45              // Minutes feeding pumps are to be turned off for
-#define SETUP_CROP1_TYPE            Lettuce
-#define SETUP_CROP1_SUBSTRATE       ClayPebbles
-#define SETUP_CROP1_SOW_DATE        DateTime(2022, 5, 21)
-#define SETUP_CROP2_TYPE            Lettuce
-#define SETUP_CROP2_SUBSTRATE       ClayPebbles
-#define SETUP_CROP2_SOW_DATE        DateTime(2022, 5, 21)
-#define SETUP_CROP3_TYPE            Lettuce
-#define SETUP_CROP3_SUBSTRATE       ClayPebbles
-#define SETUP_CROP3_SOW_DATE        DateTime(2022, 5, 21)
-#define SETUP_CROP4_TYPE            Lettuce
-#define SETUP_CROP4_SUBSTRATE       ClayPebbles
-#define SETUP_CROP4_SOW_DATE        DateTime(2022, 5, 21)
-#define SETUP_CROP5_TYPE            Lettuce
-#define SETUP_CROP5_SUBSTRATE       ClayPebbles
-#define SETUP_CROP5_SOW_DATE        DateTime(2022, 5, 21)
 
 
 Hydroponics hydroController(SETUP_PIEZO_BUZZER_PIN,
@@ -136,10 +95,7 @@ void setup() {
         #if SETUP_DATA_SD_ENABLE
             hydroController.enableDataPublishingToSDCard(F(SETUP_DATA_FILE_PREFIX));
         #endif
-
-        // TODO: Object setup
     }
-    
 
     // TODO: UI initialization, other setup options
 
--- examples/SimpleDWC/SimpleDWC.ino
+++ examples/SimpleDWC/SimpleDWC.ino
@@ -3,39 +3,45 @@
 
 #include <Hydroponics.h>
 
-#define AeratorRelayPin     4
-#define GrowLightsRelayPin  5
-#define LettuceSowDate      DateTime(2022, 5, 21)
+#define SETUP_GROW_LIGHTS_PIN       8               // Grow lights relay pin (digital)
+#define SETUP_AERATOR_PIN           7               // Aerator relay pin (digital)
+#define SETUP_FEED_RESERVOIR_SIZE   5               // Reservoir size, in default measurement units
+#define SETUP_AC_POWER_RAIL_TYPE    AC110V          // Rail power type used for AC rail (AC110V, AC220V)
 
-Hydroponics hydroController;            // Controller using default XXX TODO pin DXX, and default Wire @400kHz
+#define SETUP_CROP1_TYPE            Lettuce
+#define SETUP_CROP1_SUBSTRATE       ClayPebbles
+#define SETUP_CROP1_SOW_DATE        DateTime(2022, 5, 21)
+
+Hydroponics hydroController;            // Controller using default settings
 
 void setup() {
-    Serial.begin(115200);               // Begin Serial and Wire interfaces
-    while(!Serial) { ; }                // Wait for USB serial to connect (remove in production)
+    Serial.begin(115200);               // Begin USB Serial interface
+    while(!Serial) { ; }                // Wait for USB Serial to connect (remove in production)
 
-    // Initializes controller with default environment, no logging, eeprom, SD, or anything else
+    // Initializes controller with default environment, no logging, eeprom, SD, or anything else.
     hydroController.init();
 
-    // Adds our relay power rail as standard AC. This will manage how many active devices can be turned on at the same time.
-    auto relayPower = hydroController.addSimplePowerRail(Hydroponics_RailType_AC110V);
+    // Adds a simple relay power rail using standard AC. This will manage how many active devices can be turned on at the same time.
+    auto relayPower = hydroController.addSimplePowerRail(JOIN(Hydroponics_RailType_,SETUP_AC_POWER_RAIL_TYPE));
 
-    // Adds a 4 gallon main water reservoir, already filled with feed water.
-    auto feedWater = hydroController.addFeedWaterReservoir(4, true);
-    feedWater->setVolumeUnits(Hydroponics_UnitsType_LiqVolume_Gallons);
+    // Adds a 5 unit main water reservoir, already filled with feed water.
+    auto feedWater = hydroController.addFeedWaterReservoir(SETUP_FEED_RESERVOIR_SIZE, true);
 
-    // Add water aerator relay at AeratorRelayPin, and link it to the feed water reservoir and the relay power rail.
-    auto aerator = hydroController.addWaterAeratorRelay(AeratorRelayPin);
+    // Adds a water aerator at SETUP_AERATOR_PIN, and links it to the feed water reservoir and the relay power rail.
+    auto aerator = hydroController.addWaterAeratorRelay(SETUP_AERATOR_PIN);
     aerator->setRail(relayPower);
     aerator->setReservoir(feedWater);
 
-    // Add grow lights relay at GrowLightsRelayPin, and link it to the feed water reservoir and the relay power rail.
-    auto growLights = hydroController.addGrowLightsRelay(GrowLightsRelayPin);
-    growLights->setRail(relayPower);
-    growLights->setReservoir(feedWater);
+    // Add grow lights relay at SETUP_GROW_LIGHTS_PIN, and links it to the feed water reservoir and the relay power rail.
+    auto lights = hydroController.addGrowLightsRelay(SETUP_GROW_LIGHTS_PIN);
+    lights->setRail(relayPower);
+    lights->setReservoir(feedWater);
 
-    // Add some lettuce, set to feed on a standard 15 mins on/45 mins off timer, that we planted in clay pebbles on LettuceSowDate, and link it to the feed water reservoir.
-    auto lettuce = hydroController.addTimerFedCrop(Hydroponics_CropType_Lettuce, Hydroponics_SubstrateType_ClayPebbles, LettuceSowDate);
-    lettuce->setFeedReservoir(feedWater);
+    // Add timer fed crop set to feed on a standard 15 mins on/45 mins off timer, and links it to the feed water reservoir.
+    auto crop = hydroController.addTimerFedCrop(JOIN(Hydroponics_CropType_,SETUP_CROP1_TYPE),
+                                                JOIN(Hydroponics_SubstrateType_,SETUP_CROP1_SUBSTRATE),
+                                                SETUP_CROP1_SOW_DATE);
+    crop->setFeedReservoir(feedWater);
 
     // Launches controller into main operation.
     hydroController.launch();
